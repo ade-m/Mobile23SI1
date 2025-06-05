e83561748c6341c11b2d55c3e503d42d56c983e1
ade-m <66313509+ade-m@users.noreply.github.com>
2025-06-05 11:35:04 +0700

    Pertemuan 5
    Layout

--- app/src/main/java/edu/uph/m23si1/aplikasipertama/DashboardActivity.java
+++ app/src/main/java/edu/uph/m23si1/aplikasipertama/DashboardActivity.java
@@ -2,11 +2,8 @@ package edu.uph.m23si1.aplikasipertama;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,40 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
-    Button btnRegister;
-    EditText edtName;
+public class DashboardActivity extends AppCompatActivity {
+    LinearLayout llyProfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_dashboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        edtName = findViewById(R.id.edtName);
-        btnRegister = findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+
+        llyProfil= findViewById(R.id.llyProfil);
+        llyProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                //Toast toast = Toast.makeText(getApplicationContext(), "Tombol Register diTekan", Toast.LENGTH_LONG);
-                //toast.setGravity(Gravity.BOTTOM | Gravity.RIGHT, 0, 0);
-                //toast.show();
-                toDashboard();
+                toProfil();
             }
         });
     }
     public void toProfil(){
-        String nama = edtName.getText().toString();
+        String nama = getIntent().getStringExtra("nama");
         Intent intent = new Intent(this, ProfilActivity.class);
-        intent.putExtra("nama",nama);
-        startActivity(intent);
-    }
-
-    public void toDashboard(){
-        String nama = edtName.getText().toString();
-        Intent intent = new Intent(this, DashboardActivity.class);
         intent.putExtra("nama",nama);
         startActivity(intent);
     }
